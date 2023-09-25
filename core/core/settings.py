22e71d8eed573967efcdc60f481662aab8c169b3
arminadm <armindarabimahboub@gmail.com>
2023-09-25 20:34:00 +0330

    projected dockerized + templates added + required views added

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c&%&*_cnai7n5#26!6&ojo9szrrbvr)=zz!pe2jwy*2++q-!se
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "website",
+    "channels"
 ]
 
 MIDDLEWARE = [
---
+++ core/website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import IndexView, RoomView
+
+
+app_name = "website"
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("chat/<str:room_name>/", RoomView.as_view(), name="room"),
+]
