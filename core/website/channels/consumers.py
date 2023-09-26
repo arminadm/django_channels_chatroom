3e5bda43ce7bf243fe5b229b2b9228f21d6b6f56
arminadm <armindarabimahboub@gmail.com>
2023-09-26 12:08:06 +0330

    async chat room working fine, sync chatroom disabled

--- core/website/channels/consumers.py
+++ core/website/channels/consumers.py
@@ -3,7 +3,49 @@ from asgiref.sync import async_to_sync
 import json
 
 
-class ChatAsyncConsumer(WebsocketConsumer):
+class ChatAsyncConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # joining room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        await self.accept()
+    
+    async def disconnect(self, code):
+        # Leaving room group
+        self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+    
+    # Receive data from websocket
+    async def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        
+        # send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message
+            }
+        )
+        
+    async def chat_message(self, event):
+        message = event["message"]
+        
+        await self.send(text_data=json.dumps({
+            "message": message
+        }))
+
+
+class ChatSyncConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
